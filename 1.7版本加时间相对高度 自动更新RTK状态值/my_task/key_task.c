229a62bdffe5972563ef8756f1efc55728fbb009
xwt916140252 <50857397+xwt916140252@users.noreply.github.com>
2019-08-29 12:02:22 +0800

    update .gitignore

--- 1.7版本加时间相对高度 自动更新RTK状态值/my_task/key_task.c
+++ 1.7版本加时间相对高度 自动更新RTK状态值/my_task/key_task.c
@@ -13,75 +13,9 @@ void  KEY_TASK (void const * argument)
 
 	while (1) 
 	{
-        osDelayUntil(&xLastWakeTime,10);        
-		if(0==g_setModelFlag)
-		{
-			if(KEY_ENING)
-            {
-                osDelay(5);	
-                if(KEY_ENING)
-                {
-                    osDelay(15);	
-                    if(KEY_ENING)
-                    {
-                       xEventGroupSetBits(Key_EventGroup,KEY_SHORT);
-                    } 
-                    while(KEY_ENING)
-										{
-												while(KEY_ENING)
-												{
-													osDelay(10);	
-												}
-												osDelay(60);	
-										}
-                } 
-            }    
-		}
-		else
-		{
-			if(KEY_ENING)
-			{
-				osDelay(5);	
-				if(KEY_ENING)
-				{
-						osDelay(15);	
-						if(KEY_ENING)
-						{              
-								n=0;
-								while(KEY_ENING)
-								{
-										osDelay(10);	
-										n++;
-										if(100<=n)
-										{
-											n=210;
-											break;
-										}
-								}
-								
-								while(KEY_ENING)
-								{
-										while(KEY_ENING)
-										{
-											 osDelay(10);	
-										}
-										osDelay(60);	
-								}
-														
-								//--------------------------------------------------------------
-								//放到后面 这样长按时 以松手后出现反应 这样人性化
-								if(200<=n)
-								{
-										xEventGroupSetBits(Key_EventGroup,KEY_LONG);
-								}
-								else
-								{
-										xEventGroupSetBits(Key_EventGroup,KEY_SHORT);
-								}	
-						}
-					}
-			}	
-		}        
+    
+		
+	     
 	 }				
 }
 
