7fd18393adf07da91ea1009a54d494f2a8d73c34
xwt916140252 <50857397+xwt916140252@users.noreply.github.com>
2019-08-29 09:05:22 +0800

    Revert "1.7"
    This reverts commit fc0b23d20050042ef8a02c729a852f73c53605d6.

--- 1.7版本精简版 -  移除DS18B20/1.8 测试版，简化程序，加自动升级程序/Output/psdk_md5.d
+++
@@ -1,5 +0,0 @@
-..\..\output\psdk_md5.o: ..\..\mid_psdk_core\src\psdk_md5.c
-..\..\output\psdk_md5.o: D:\SoftwareCatalog\keil5\ARM\ARMCC\Bin\..\include\stdlib.h
-..\..\output\psdk_md5.o: ..\..\mid_psdk_core\inc\psdk_md5.h
-..\..\output\psdk_md5.o: D:\SoftwareCatalog\keil5\ARM\ARMCC\Bin\..\include\stddef.h
-..\..\output\psdk_md5.o: D:\SoftwareCatalog\keil5\ARM\ARMCC\Bin\..\include\string.h
